70699d815d2265d7c3aad069cbafa89ab0e9db5e
smarist <stmarisnwokike@gmail.com>
2022-03-29 05:59:33 +1200

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import './index.css';
+import Nav from "../src/components/nav"
+
+export default function App() {
+  return (
+    <Nav/>
+  );
+}
+
+
---
+++ src/components/nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import NavImg from "../images/nav-image.png"
+
+
+
+export default function Nav(){
+    return (
+        <nav>
+            <img src={NavImg}></img>
+            <h1>my travel journal</h1>
+        </nav>
+    )
+   
+}
